70b88c67c86ead44115625964225176c4d402a98
Sanjit Raman <sanjit.raman@gmail.com>
2020-03-23 13:27:11 +0000

    Removed support file

--- support-model.py
+++
@@ -1,6 +0,0 @@
-print("looka' me go!")
-
-# I can even make changes on github, and sync them to my desktop.
-
-# This is a change I made to add fatalities to the program.
-#Whoops I had to fix something
